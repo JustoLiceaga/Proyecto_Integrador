76c019c609e44d45312a6da49f63688dd6152c40
MarcoMambrettiUdesa <mmambretti@udesa.edu.ar>
2025-04-13 21:20:25 -0300

    barra de busqueda

--- proyecto/routes/index.js
+++ proyecto/routes/index.js
@@ -14,7 +14,7 @@ router.get('/editar', indexController.editar)
 
 router.get('/profile', indexController.profile)
 
-router.get('/serch-results/:producto?', indexController.search)
+router.get('/serch-results', indexController.search)
 
 
 module.exports = router;
