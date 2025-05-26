3e6294f2a0ef62e1d7df5b77f35bdfda18bd2d1f
valencag332 <vcagnolattimantaras@gmail.com>
2025-05-26 10:45:09 -0300

    modificaciones en index.ejs y indexcontroller para hacer el display de los productos de la database en el menu

--- proyecto/controllers/indexController.js
+++ proyecto/controllers/indexController.js
@@ -4,7 +4,25 @@ let bcrypt = require ('bcryptjs');
 
 const indexController = {
     index: function(req, res) {
-        res.render('index', { usuario: data.usuario, producto: data.productos});
+        
+      db.Producto.findAll({
+        include: [
+          {
+            model: db.Usuario,
+            as: 'usuario'
+          },
+          {
+            model: db.Producto,
+            as: 'producto'
+          }]
+      })
+      .then(function(productos) {
+        res.render('index', { usuario: usuario, producto: producto });
+      })
+      .catch(function(error) {
+        res.send(error);
+      });
+      // esto estaba antes: (db vieja) res.render('index', { usuario: data.usuario, producto: data.productos});
       }
 }
 
