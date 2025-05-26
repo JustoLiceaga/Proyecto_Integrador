fbf076e0ecec0d402879ef47390f8297f1e3bac1
valencag332 <vcagnolattimantaras@gmail.com>
2025-05-26 16:06:16 -0300

    detalles de cada producto hecho

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -1,50 +1,60 @@
 let db = require("../database/models");
-let bcrypt = require ('bcryptjs');
+let bcrypt = require('bcryptjs');
 const producto = db.Producto
 const usuario = db.Usuario;
 
-const productController ={
-    detalle: function(req, res) {
-        id = req.params.id;
+const productController = {
+  detalle: function (req, res) {
+    id = req.params.id;
 
-        producto.findOne({
-          where: { id: id },
-			  include: { 
+    producto.findOne({
+      where: { id: id },
+      include: [
+        {
           model: usuario,
-          as: 'usuario'} })
-        .then(function (producto) {
-             return res.render("product", {producto: producto})
-            })
+          as: 'usuario'
+        },
+        {
+          model: db.Comentario,
+          as: 'comentarios'
+        }
+      ]
+    })
+      .then(function (producto) {
+        return res.render("product", { producto: producto })
+      })
+  },
+
+
+
+
+  editar: function (req, res) {
+    res.render('product-add', { usuario: data.usuario });
+  },
+  search: function (req, res) {
+
+    busqueda = req.query.search;
+    producto.findAll({
+      where: {
+        nombre: { [op.like]: '%' + busqueda + '%' }
       },
-
-
-
-
-      editar: function(req, res) {
-        res.render('product-add', { usuario: data.usuario });
-      },
-      search: function(req, res) {
-
-        busqueda = req.query.search;
-        producto.findAll({
-            where: {
-                nombre: {[op.like]: '%' + busqueda + '%'} },
-                include: [{
-                model: usuario,
-                as: 'usuario',
-                attributes: ['usuario'] // Solo trae el nombre de usuario
-            }]
-            })
-            .then(function (producto) {
-              if (!producto) {
-                return res.send('No hay resultados para su criterio de búsqueda');
-              }else{
-                return res.render("search-results", {productos: producto})}
-            })
-            .catch(function(error){
-                return res.send(error);
-            })
-      }
+      include: [{
+        model: usuario,
+        as: 'usuario',
+        attributes: ['usuario'] // Solo trae el nombre de usuario
+      }]
+    })
+      .then(function (producto) {
+        if (!producto) {
+          return res.send('No hay resultados para su criterio de búsqueda');
+        } else {
+          return res.render("search-results", { productos: producto })
+        }
+      })
+      .catch(function (error) {
+        return res.send(error);
+      })
+  }
 }
 
 
--- proyecto/routes/product.js
+++ proyecto/routes/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController')
 
-router.get('/detalle/:detalle', productController.detalle);
+router.get('/detalle/:id', productController.detalle);
 
 router.get('/editar', productController.editar)
 
